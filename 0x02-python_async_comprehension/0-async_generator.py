4bfd4f6b9d73a18a272587afda1396f405fc5a0e
maramilod <marammilod03@gmail.com>
2024-05-07 15:23:39 +0100

    add

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+import random
+import asyncio
+from typing import List
+
+async def async_generator() -> List[float]:
+    """
+    An asynchronous generator that yields a list of random floats over time.
+    """
+    li: List[float] = []
+    for _ in range(10):
+        await asyncio.sleep(1)
+        li.append(random.uniform(0, 10))
+        yield li[-1]
+
+async def print_yielded_values():
+    async for i in async_generator():
+        print(i)
+
+# Run the event loop
+if __name__ == "__main__":
+    asyncio.run(print_yielded_values())
+
